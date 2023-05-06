ba3e2f59a93062ff76f19fb4c5a99fef58617b99
Anna-Mamedova <mamedi289@gmail.com>
2023-05-06 17:42:43 +0300

    feat: add solution for 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const { copyFile, mkdir, readdir, rm } = require('fs/promises')
+const path = require('path')
+const folderPath = path.join(__dirname, 'files')
+const copyFolderPath = path.join(__dirname, 'files-copy')
 
+async function copyFilesToFolder() {
+    await rm(copyFolderPath, { recursive: true })
+    await mkdir(copyFolderPath, { recursive: true })
+
+    const files = await readdir(folderPath, { withFileTypes: true })
+     
+    for(const file of files){
+        try {
+            await copyFile(path.join(folderPath, file.name), path.join(copyFolderPath, file.name))
+            console.log(`\u001B[32mФайл ${file.name} был скопирован в папку files-copy\u001B[0m`)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+}
+
+copyFilesToFolder()
