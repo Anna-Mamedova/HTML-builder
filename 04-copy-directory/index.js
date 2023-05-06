647a49f7d68660e92cde1618cffa7300565b488b
Anna-Mamedova <mamedi289@gmail.com>
2023-05-06 19:47:56 +0300

    fix: fix update files-copy bug

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,20 +1,29 @@
-const { copyFile, mkdir, readdir, rm } = require('fs/promises')
+const { copyFile, mkdir, readdir, unlink } = require('fs/promises')
 const path = require('path')
 const folderPath = path.join(__dirname, 'files')
 const copyFolderPath = path.join(__dirname, 'files-copy')
 
 async function copyFilesToFolder() {
-    await rm(copyFolderPath, { recursive: true })
     await mkdir(copyFolderPath, { recursive: true })
 
+    const copyFolder = await readdir(copyFolderPath, { withFileTypes: true })
+
+    for(const file of copyFolder){
+        try {
+            await unlink(path.join(copyFolderPath, file.name))
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     const files = await readdir(folderPath, { withFileTypes: true })
-     
+    
     for(const file of files){
         try {
             await copyFile(path.join(folderPath, file.name), path.join(copyFolderPath, file.name))
             console.log(`\u001B[32mФайл ${file.name} был скопирован в папку files-copy\u001B[0m`)
         } catch (error) {
-            console.error(error)
+            console.log(error)
         }
     }
 
